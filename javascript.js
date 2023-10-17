bfef48c199214f5722668d1097dae9c42d22b8b0
renad <renaad.abu.taha@gmail.com>
2023-10-17 21:44:43 +0300

    second commit

--- javascript.js
+++ javascript.js
@@ -1,63 +1,57 @@
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+ import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-database.js";
+    
+ const firebaseConfig = {
+    apiKey: "AIzaSyDDd4fWG6CTtjRcbsP-MDBKAToM2ya9IUs",
+          authDomain: "carma-blue-6991e.firebaseapp.com",
+          projectId: "carma-blue-6991e",
+          storageBucket: "carma-blue-6991e.appspot.com",
+          messagingSenderId: "288486090590",
+          appId: "1:288486090590:web:b05a34e6fdecee9fd891cf"
+};
+      
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app); // initialize the firebase realtime database
+
 const form = document.getElementById('form');
 const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
-// What happens when we "submit" our form by clicking the button
 form.addEventListener('submit', e => {
-    // checks inputs and sets errors if needed
+    e.preventDefault();
     checkInputs();
-
-    // check if there are any error messages to stop the button from submitting
-    const errorElements = document.querySelectorAll('.error');
-    if (errorElements.length === 0) {
-        //allows the form to submit if there is no errors
-        return; 
-    }
-        // Prevent form submission if there are errors
-        e.preventDefault(); 
 });
 
 function checkInputs() {
-    // Getting values from the input and trim to remove whitespace
-    const firstnameValue = firstname.value.trim();
-    const lastnameValue = lastname.value.trim();
-    const emailValue = email.value.trim();
-    const phoneValue = phone.value.trim();
-
-    // Conditions and error messages sent when our conditions are not met
-    if (firstnameValue === '') {
-        setErrorFor(firstname, 'Name cannot be blank');
-    } else if (!isValidFirstName(firstnameValue)) {
-        setErrorFor(firstname, 'Invalid name [only letters allowed]');
-    } else {
-        setSuccessFor(firstname);
+    validateFirstName();
+    validateLastName();
+    validateEmail();
+    validatePhone();
+    // After input validation, save the data if there are no errors
+    if (document.querySelectorAll('.error').length === 0) {
+        saveFormData();
     }
+}
 
-    if (lastnameValue === '') {
-        setErrorFor(lastname, 'Name cannot be blank');
-    } else if (!isValidLastName(lastnameValue)) {
-        setErrorFor(lastname, 'Invalid name [only letters allowed]');
-    } else {
-        setSuccessFor(lastname);
-    }
+function saveFormData() {
+    const formData = {
+        firstname: firstname.value.trim(),
+        lastname: lastname.value.trim(),
+        email: email.value.trim(),
+        phone: phone.value.trim()
+    };
 
-    if (emailValue === '') {
-        setErrorFor(email, 'Email cannot be blank');
-    } else if (!isValidEmail(emailValue)) {
-        setErrorFor(email, 'Invalid email address');
-    } else {
-        setSuccessFor(email);
-    }
+    const dataRef = ref(database, 'formResponses');
 
-    if (phoneValue === '') {
-        setErrorFor(phone, 'Phone cannot be blank');
-    } else if (!isValidPhone(phoneValue)) {
-        setErrorFor(phone, 'Invalid [min number of digits=9, max=15]');
-    } else {
-        setSuccessFor(phone);
-    }
+    push(dataRef, formData)
+        .then(() => {
+            console.log('Data saved successfully');
+        })
+        .catch(error => {
+            console.error('Error saving data:', error);
+        });
 }
 
 // Function to set error
@@ -68,16 +62,61 @@ function setErrorFor(input, message) {
     // Adds error message in our small tag
     small.innerText = message;
 
-    //shows error(red color and exclamation mark)
+    // Shows error (red color and exclamation mark)
     information.classList.remove('success');
     information.classList.add('error');
 }
 
-//shows success(green color and check) when condition met
+// Shows success (green color and check) when condition met
 function setSuccessFor(input) {
     const information = input.parentElement;
     information.classList.remove('error');
     information.classList.add('success');
+}
+
+function validateFirstName() {
+    const firstnameValue = firstname.value.trim();
+    if (firstnameValue === '') {
+        setErrorFor(firstname, 'Name cannot be blank');
+    } else if (!isValidFirstName(firstnameValue)) {
+        setErrorFor(firstname, 'Invalid name [only letters allowed]');
+    } else {
+        setSuccessFor(firstname);
+    }
+}
+
+function validateLastName() {
+    const lastnameValue = lastname.value.trim();
+    if (lastnameValue === '') {
+        setErrorFor(lastname, 'Name cannot be blank');
+    } else if (!isValidLastName(lastnameValue)) {
+        setErrorFor(lastname, 'Invalid name [only letters allowed]');
+    } else {
+        setSuccessFor(lastname);
+    }
+}
+
+function validateEmail() {
+    const emailValue = email.value.trim();
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isValidEmail(emailValue)) {
+        setErrorFor(email, 'Invalid email address');
+    } else {
+        setSuccessFor(email);
+    }
+}
+
+function validatePhone() {
+    const phoneValue = phone.value.trim();
+    if (phoneValue === '') {
+        setErrorFor(phone, 'Phone cannot be blank');
+    } else if (!isValidPhone(phoneValue)) {
+        setErrorFor(phone, 'Invalid [min number of digits=9, max=15]');
+    } else {
+        setSuccessFor(phone);
+    }
+
 }
 
 function isValidFirstName(firstname) {
@@ -89,11 +128,11 @@ function isValidLastName(lastname) {
 }
 
 function isValidEmail(email) {
-    //  regex for email validation
+    // Regex for email validation
     return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
 }
 
 function isValidPhone(phone) {
-    //  regex for phone validation (max 15 digits)
+    // Regex for phone validation (max 15 digits)
     return /^(\+\d{1,4}\s?)?(\d{9,15})$/.test(phone);
 }
